243d31692a444373029cbd0a2107d86c96e4891b
Thies Nieborg <thies.nieborg@student.uva.nl>
2022-12-01 07:27:44 +0100

    Hyppppeeee!!!! ❤️❤️

--- 01/index.ts
+++ 01/index.ts
@@ -8,7 +8,14 @@ const part1 = (input: string) => {
 	const start = now();
 	let result;
 
-	const data = input.split("\n");
+	const data = input.split("\n\n").map((x) => x.split("\n"));
+	const summed = data.map((x) => {
+		return x.reduce((a, b) => {
+			return a + parseInt(b);
+		}, 0);
+	});
+
+	result = Math.max(...summed);
 
 	const end = now();
 	console.log("Execution time: ~%dms", (end - start).toFixed(3));
@@ -24,7 +31,16 @@ const part2 = (input: string) => {
 	const start = now();
 	let result = 0;
 
-	const data = input.split("\n");
+	const data = input.split("\n\n").map((x) => x.split("\n"));
+	const summed = data.map((x) => {
+		return x.reduce((a, b) => {
+			return a + parseInt(b);
+		}, 0);
+	});
+
+	summed.sort((a, b) => b - a);
+	const firstThree = summed.slice(0, 3);
+	result = firstThree.reduce((a, b) => a + b, 0);
 
 	const end = now();
 	console.log("Execution time: ~%dms", (end - start).toFixed(3));
