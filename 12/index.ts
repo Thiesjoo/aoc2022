3ba38c3de01f3f00d5ba6e0b93a5586744f0edb5
Thies <git@thies.dev>
2022-12-12 09:13:22 +0100

    💻compoooter do brrrrrrrrrrrrrrrrrrrrrrrr

--- 12/index.ts
+++ 12/index.ts
@@ -4,8 +4,12 @@ function locFromMap(map: string[][], loc: { x: number; y: number }) {
 	return map[loc.y][loc.x];
 }
 
-function compareChars(map: string[][], loc1: { x: number; y: number }, loc2: { x: number; y: number }) {
-	// Check if we are out of bounds
+function compareChars(
+	map: string[][],
+	loc1: { x: number; y: number },
+	loc2: { x: number; y: number },
+	part2: boolean = false
+) {
 	if (loc1.x < 0 || loc1.x >= map[0].length || loc1.y < 0 || loc1.y >= map.length) {
 		return false;
 	}
@@ -23,9 +27,42 @@ function compareChars(map: string[][], loc1: { x: number; y: number }, loc2: { x
 		chars1 = "a";
 	}
 
-	console.log(chars1, chars2, chars1.charCodeAt(0) + 1 >= chars2.charCodeAt(0));
-
 	return chars1.charCodeAt(0) + 1 >= chars2.charCodeAt(0);
+}
+
+function findPath(
+	data: string[][],
+	startPos: { x: number; y: number },
+	endPos: { x: number; y: number },
+	part2: boolean = false
+) {
+	const visited = new Set<string>();
+	const queue = [{ location: startPos, length: 0 }];
+	while (queue.length > 0) {
+		let { location: current, length } = queue.shift()!;
+
+		if (current.x === endPos.x && current.y === endPos.y) {
+			return length;
+		}
+
+		let next = [
+			{ x: current.x + 1, y: current.y },
+			{ x: current.x - 1, y: current.y },
+			{ x: current.x, y: current.y + 1 },
+			{ x: current.x, y: current.y - 1 },
+		].filter((x) => {
+			if (visited.has(`${x.x},${x.y}`)) {
+				return false;
+			}
+			return compareChars(data, current, x, part2);
+		});
+
+		next.forEach((x) => {
+			visited.add(`${x.x},${x.y}`);
+			queue.push({ location: x, length: length + 1 });
+		});
+	}
+	return -10000;
 }
 
 // Part 1
@@ -37,7 +74,6 @@ const part1 = (input: string) => {
 	let result = 0;
 
 	const data = input.split("\n").map((x) => x.split(""));
-	console.log(data);
 
 	let startPos = { x: 0, y: 0 };
 	let endPos = { x: 0, y: 0 };
@@ -54,37 +90,7 @@ const part1 = (input: string) => {
 		});
 	});
 
-	console.log(startPos, endPos);
-
-	const visited = new Set<string>();
-	const queue = [{ location: startPos, length: 0 }];
-	while (queue.length > 0) {
-		let { location: current, length } = queue.shift()!;
-
-		console.log("current", current, length);
-		if (current.x === endPos.x && current.y === endPos.y) {
-			console.log("reached the end");
-			console.log(length);
-			break;
-		}
-
-		let next = [
-			{ x: current.x + 1, y: current.y },
-			{ x: current.x - 1, y: current.y },
-			{ x: current.x, y: current.y + 1 },
-			{ x: current.x, y: current.y - 1 },
-		].filter((x) => {
-			if (visited.has(`${x.x},${x.y}`)) {
-				return false;
-			}
-			return compareChars(data, current, x);
-		});
-
-		next.forEach((x) => {
-			visited.add(`${x.x},${x.y}`);
-			queue.push({ location: x, length: length + 1 });
-		});
-	}
+	result = findPath(data, startPos, endPos);
 
 	const end = now();
 	console.log("Execution time: ~%dms", (end - start).toFixed(3));
@@ -100,9 +106,24 @@ const part2 = (input: string) => {
 	const start = now();
 	let result = 0;
 
-	const data = input.split("\n");
-	console.log(data);
+	const data = input.split("\n").map((x) => x.split(""));
 
+	let startPos = [] as { x: number; y: number }[];
+	let endPos = { x: 0, y: 0 };
+
+	data.forEach((x, i) => {
+		x.forEach((y, j) => {
+			if (y === "E") {
+				endPos.x = j;
+				endPos.y = i;
+			} else if (y === "a" || y === "S") {
+				startPos.push({ x: j, y: i });
+			}
+		});
+	});
+
+	const results = startPos.map((x) => findPath(data, x, endPos, true)).filter((x) => x >= 0);
+	result = Math.min(...results);
 	const end = now();
 	console.log("Execution time: ~%dms", (end - start).toFixed(3));
 
