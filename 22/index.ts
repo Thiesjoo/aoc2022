cccf12a1bc74e36bcdcc61bffb38437085300b53
Thies <git@thies.dev>
2022-12-22 16:17:02 +0100

    😍part 2 is hell

--- 22/index.ts
+++ 22/index.ts
@@ -11,7 +11,7 @@ const outOfBounds = (newLocation: any, map: any) =>
 
 // Part 1
 // ======
-// ~0 ms - answer: 0
+// ~18 ms - answer: 64256
 
 const part1 = (input: string) => {
 	const start = now();
@@ -21,11 +21,8 @@ const part1 = (input: string) => {
 	const [mapString, instructions] = (` `.repeat(demo ? 8 : 50) + input).split("\n\n");
 	const map = mapString.split("\n").map((line) => [...line]);
 
-	console.log(map[0]);
-
 	const startLocation = map[0].findIndex((x) => x === OPEN);
 	let current = { x: startLocation, y: 0 };
-	console.log("start location", current);
 	const directions = {
 		U: { x: 0, y: -1 },
 		D: { x: 0, y: 1 },
@@ -41,13 +38,11 @@ const part1 = (input: string) => {
 			let nowDir = directionsLoop.indexOf(direction);
 			nowDir = (nowDir + 1) % directionsLoop.length;
 			direction = directionsLoop[nowDir];
-			console.log("Switched right");
 		} else if (instruction === "L") {
 			// turn left
 			let nowDir = directionsLoop.indexOf(direction);
 			nowDir = mod(nowDir - 1, directionsLoop.length);
 			direction = directionsLoop[nowDir];
-			console.log("Switched left");
 		} else {
 			let numbers = instruction;
 			// Check if next instructions is number
@@ -62,23 +57,19 @@ const part1 = (input: string) => {
 
 				if (!outOfBounds(newLocation, map) && map[newLocation.y][newLocation.x] === OPEN) {
 					current = newLocation;
-					console.log("new location", current);
 				} else if (outOfBounds(newLocation, map) || map[newLocation.y][newLocation.x] !== CLOSED) {
-					console.log(direction, "Should wrap around", newLocation, current);
 					newLocation = { x: current.x, y: current.y };
 					while (
 						!outOfBounds(newLocation, map) &&
 						map[newLocation.y][newLocation.x] !== " " &&
 						map[newLocation.y][newLocation.x] !== undefined
 					) {
-						console.log(newLocation, map[newLocation.y][newLocation.x]);
 						newLocation.x -= directions[direction].x;
 						newLocation.y -= directions[direction].y;
 					}
 					newLocation.x += directions[direction].x;
 					newLocation.y += directions[direction].y;
 					if (map[newLocation.y][newLocation.x] === "#") {
-						console.log("Hit a wall", newLocation);
 						break;
 					}
 					current = newLocation;
@@ -112,9 +103,141 @@ const part2 = (input: string) => {
 	const start = now();
 	let result = 0;
 
-	const data = input.split("\n");
-	console.log(data);
+	const demo = false;
+	const [mapString, instructions] = (` `.repeat(demo ? 8 : 50) + input).split("\n\n");
+	const map = mapString.split("\n").map((line) => [...line]);
 
+	const startLocation = map[0].findIndex((x) => x === OPEN);
+	let current = { x: startLocation, y: 0 };
+	const directions = {
+		U: { x: 0, y: -1 },
+		D: { x: 0, y: 1 },
+		L: { x: -1, y: 0 },
+		R: { x: 1, y: 0 },
+	};
+	const directionsLoop = ["U", "R", "D", "L"] as (keyof typeof directions)[];
+	let direction = "R" as keyof typeof directions;
+
+	for (let i = 0; i < instructions.length; i++) {
+		const instruction = instructions[i];
+		if (instruction === "R") {
+			let nowDir = directionsLoop.indexOf(direction);
+			nowDir = (nowDir + 1) % directionsLoop.length;
+			direction = directionsLoop[nowDir];
+		} else if (instruction === "L") {
+			// turn left
+			let nowDir = directionsLoop.indexOf(direction);
+			nowDir = mod(nowDir - 1, directionsLoop.length);
+			direction = directionsLoop[nowDir];
+		} else {
+			let numbers = instruction;
+			// Check if next instructions is number
+			if (!isNaN(parseInt(instructions[i + 1]))) {
+				numbers += instructions[i + 1];
+				i++;
+			}
+
+			const steps = +numbers;
+			step: for (let j = 0; j < steps; j++) {
+				let newLocation = { x: current.x + directions[direction].x, y: current.y + directions[direction].y };
+
+				if (!outOfBounds(newLocation, map) && map[newLocation.y][newLocation.x] === OPEN) {
+					current = newLocation;
+				} else if (outOfBounds(newLocation, map) || map[newLocation.y][newLocation.x] !== CLOSED) {
+					if (!outOfBounds(newLocation, map) && map[newLocation.y][newLocation.x] === "#") {
+						break;
+					}
+
+					const { x: px, y: py } = current;
+					let ny = -1;
+					let nx = -1;
+					let nfacing = -1;
+
+					switch (direction) {
+						case "R": //right
+							if (py <= 50) {
+								nfacing = 2;
+								ny = 151 - py;
+								nx = 100;
+							} else if (py <= 100) {
+								nfacing = 3;
+								ny = 50;
+								nx = 50 + py;
+							} else if (py <= 150) {
+								nfacing = 2;
+								ny = 151 - py;
+								nx = 150;
+							} else {
+								nfacing = 3;
+								ny = 150;
+								nx = py - 100;
+							}
+							break;
+						case "D": //down
+							if (px <= 50) {
+								nfacing = 1;
+								ny = 1;
+								nx = px + 100;
+							} else if (px <= 100) {
+								nfacing = 2;
+								ny = px + 100;
+								nx = 50;
+							} else {
+								nfacing = 2;
+								ny = px - 50;
+								nx = 100;
+							}
+							break;
+						case "L": //left
+							if (py <= 50) {
+								nfacing = 0;
+								ny = 151 - py;
+								nx = 1;
+							} else if (py <= 100) {
+								nfacing = 1;
+								ny = 101;
+								nx = py - 50;
+							} else if (py <= 150) {
+								nfacing = 0;
+								ny = 151 - py;
+								nx = 51;
+							} else {
+								nfacing = 1;
+								ny = 1;
+								nx = py - 100;
+							}
+							break;
+						case "U": //up
+							if (px <= 50) {
+								nfacing = 0;
+								ny = px + 50;
+								nx = 51;
+							} else if (px <= 100) {
+								nfacing = 0;
+								ny = px + 100;
+								nx = 1;
+							} else {
+								nfacing = 3;
+								ny = 200;
+								nx = px - 100;
+							}
+							break;
+					}
+
+					if (map[ny][nx] === CLOSED) {
+						break;
+					}
+					if (map[ny][nx] === OPEN) {
+						const test = ["R", "D", "L", "U"] as ("R" | "D" | "L" | "U")[];
+						direction = test[nfacing];
+						current = { x: nx, y: ny };
+					}
+				}
+			}
+
+			console.assert(false);
+		}
+	}
 	const end = now();
 	console.log("Execution time: ~%dms", (end - start).toFixed(3));
 
