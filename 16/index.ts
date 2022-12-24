2f99b7607a8a0cdc0c44772c0a3cfb6341e1552b
Thies <git@thies.dev>
2022-12-24 16:28:39 +0100

    ✨✨✨✨✨✨✨ done with day 1666666

--- 16/index.ts
+++ 16/index.ts
@@ -1,25 +1,62 @@
 import { default as now } from "performance-now";
 
-// Part 1
-// ======
-// ~0 ms - answer: 0
+// Hehe this is not cheating rigghhttttt
 
-const part1 = (input: string) => {
-	const start = now();
-	let result = 0;
+//@ts-ignore
+import v8 from "v8";
+v8.setFlagsFromString("--stack-size=20000");
 
+const BFS = (start: string, end: string, graph: Map<string, string[]>) => {
+	let queue = [] as string[][];
+	let visited = new Set<string>();
+
+	if (start === end) {
+		return [start];
+	}
+
+	queue.push([start]);
+
+	while (queue.length > 0) {
+		let path = queue.shift()!;
+		let node = path[path.length - 1];
+
+		if (!visited.has(node)) {
+			visited.add(node);
+			let neighbors = graph.get(node)!;
+			for (let i = 0; i < neighbors.length; i++) {
+				let neighbor = neighbors[i];
+				if (neighbor === end) {
+					return path.concat(neighbor);
+				} else {
+					queue.push(path.concat(neighbor));
+				}
+			}
+		}
+	}
+
+	return [];
+};
+
+const constructDistanceMap = (graph: Map<string, string[]>) => {
+	const distanceMap = new Map<string, number>();
+	[...graph.keys()].forEach((start) => {
+		[...graph.keys()].forEach((end) => {
+			if (!distanceMap.has(`${start}-${end}`)) {
+				const path = BFS(start, end, graph);
+				distanceMap.set(`${start}-${end}`, path.length - 1);
+			}
+		});
+	});
+	return distanceMap;
+};
+
+const parseInput = (input: string) => {
 	let graph = new Map<string, string[]>();
 	let flowRates = new Map<string, number>();
 
-	const interestingNodes = new Set<string>(["AA"]);
+	const interestingNodes = new Set<string>();
 
 	input.split("\n").forEach((x) => {
-		// Write a regex to match this example:
-		// Valve JJ has flow rate=21; tunnel leads to valve II, DD
-
-		// Then use the regex to extract the data
-		// Then use the data to build the graph
-
 		const [_, valve, __, ___, flowRate, ____, _____, ______, _______, ...tunnels] = x.split(" ");
 		const rate = +flowRate.split("=")[1].replace(";", "");
 
@@ -29,13 +66,56 @@ const part1 = (input: string) => {
 		);
 		flowRates.set(valve, rate);
 
-		if (rate > 0) {
-			interestingNodes.add(valve);
-		}
+		if (rate > 0) interestingNodes.add(valve);
 	});
-	console.log(flowRates, graph, interestingNodes);
+	return { graph, flowRates, interestingNodes };
+};
 
-	// Plan of attack
+function rates(
+	distanceMap: Map<string, number>,
+	current: string,
+	minutesLeft: number,
+	nodesLeft: string[],
+	opened = {} as { [key: string]: number }
+) {
+	const allRates = [opened];
+
+	for (let i = 0; i < nodesLeft.length; i++) {
+		const x = nodesLeft[i];
+
+		let minutesLeftIfTraveled = minutesLeft - distanceMap.get(`${current}-${x}`)! - 1;
+		if (minutesLeftIfTraveled < 1) {
+			continue;
+		}
+
+		let newOpened = { ...opened };
+		newOpened[x] = minutesLeftIfTraveled;
+
+		const newNodesLeft = [...nodesLeft];
+		newNodesLeft.splice(i, 1);
+		allRates.push(...rates(distanceMap, x, minutesLeftIfTraveled, newNodesLeft, newOpened));
+	}
+
+	return allRates;
+}
+
+// Part 1
+// ======
+// ~822 ms - answer: 2056
+
+const part1 = (input: string) => {
+	const start = now();
+
+	const { graph, flowRates, interestingNodes } = parseInput(input);
+	const distanceMap = constructDistanceMap(graph);
+	const allRates = rates(distanceMap, "AA", 30, [...interestingNodes]);
+
+	const maxPressure = allRates.map((x) =>
+		Object.entries(x).reduce((a, [node, minute]) => a + minute * flowRates.get(node)!, 0)
+	);
+
+	maxPressure.sort((a, b) => b - a);
+	let result = maxPressure[0];
 
 	const end = now();
 	console.log("Execution time: ~%dms", (end - start).toFixed(3));
@@ -45,14 +125,42 @@ const part1 = (input: string) => {
 
 // Part 2
 // ======
-// ~0 ms - answer: 0
+// ~16000 ms - answer: 2513
 
 const part2 = (input: string) => {
 	const start = now();
 	let result = 0;
 
-	const data = input.split("\n");
-	console.log(data);
+	const { graph, flowRates, interestingNodes } = parseInput(input);
+	const distanceMap = constructDistanceMap(graph);
+	const allRates = rates(distanceMap, "AA", 26, [...interestingNodes]);
+
+	const getScoreForPath = (path: { [key: string]: number }) => {
+		return Object.entries(path).reduce((acc, [key, value]) => acc + flowRates.get(key)! * value, 0);
+	};
+
+	const maxScoreForPath = {} as { [key: string]: number };
+
+	allRates.forEach((x) => {
+		const path = Object.keys(x).sort().join("-");
+		if (!maxScoreForPath[path]) {
+			maxScoreForPath[path] = -10000000;
+		}
+		maxScoreForPath[path] = Math.max(maxScoreForPath[path], getScoreForPath(x));
+	});
+
+	Object.keys(maxScoreForPath).forEach((path1) => {
+		Object.keys(maxScoreForPath).forEach((path2) => {
+			// Check if there is no intersection between path1 and path2
+			const path1Split = path1.split("-");
+			const path2Split = path2.split("-");
+			const intersection = path1Split.some((x) => path2Split.includes(x));
+
+			if (!intersection) {
+				result = Math.max(result, maxScoreForPath[path1] + maxScoreForPath[path2]);
+			}
+		});
+	});
 
 	const end = now();
 	console.log("Execution time: ~%dms", (end - start).toFixed(3));
